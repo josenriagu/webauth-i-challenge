0637facbbe0197a147d6e4cec8125cdd5120eea0
Josemaria <josemarianriagu25@gmail.com>
2019-07-22 20:59:43 +0800

    define POST new user api

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const server = require('./server');
 
 const port = process.env.PORT || 9009;
 
+server.get('/', (req, res) => {
+   try {
+      res.status(200).json('You have seen the light. Shine on!')
+   } catch (error) {
+      res.status(500).json('Oops! We missed that. Hang on, let\'s fix it together')
+   }
+});
+
 server.listen(port, () => {
    console.log(`Server locked and loaded on port ${port}!!`)
 });
--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 
 const server = express();
+const Users = require('./data/helpers/dbModel');
+const mw = require('./data/helpers/middleware');
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
 
-server.get('/', (req, res) => {
+server.get('/api/users', async (req, res) => {
    try {
-      res.status(200).json('You have seen the light. Shine on!')
+      const user = await Users.get();
+      res.status(200).json(user);
    } catch (error) {
+      res.status(500).json('Oops! We missed that. Hang on, let\'s fix it together');
+   };
+});
+
+server.post('/auth/register', mw.validateUser, async (req, res) => {
+   const { password } = req.body;
+   const hashed = bcrypt.hashSync(password, 16);
+   req.body.password = hashed;
+   try {
+      const { id, username } = await Users.insertUser(req.body);
+      res.status(201).json({ message: `Hooray! Welcome Aboard, ${username}!!`, id, username })
+   }
+   catch (error) {
       res.status(500).json('Oops! We missed that. Hang on, let\'s fix it together')
    }
-});
+})
 
 module.exports = server;
