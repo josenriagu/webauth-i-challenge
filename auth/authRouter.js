521c4c44271b6d8617bb477401f2b5adbdb93c4b
Josemaria <josemarianriagu25@gmail.com>
2019-07-24 05:45:26 +0800

    refactor routers

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -1,15 +1,44 @@
 const express = require('express')
-
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
-router.get('/users', (req, res) => {
-   const { id, username } = req.user;
+const mw = require('../data/helpers/middleware');
+const Users = require('../data/helpers/dbModel');
 
+router.post('/register', async (req, res) => {
+   const { password } = req.body;
+   const hashed = bcrypt.hashSync(password, 16);
+   req.body.password = hashed;
    try {
-      res.status(200).json({ id, username });
+      const { id, username } = await Users.insertUser(req.body);
+      res.status(201).json({ message: `Hooray! Welcome Aboard, ${username}!!`, id, username })
+   }
+   catch (error) {
+      res.status(500).json('Oops! We missed that. Hang on, let\'s fix it together');
+   };
+});
+
+router.post('/logout', (req, res) => {
+   if (req.session) {
+      req.session.destroy(err => {
+         if (err) {
+            res.send('Oops! you can\'t leave now. Please try again in a short while');
+         } else {
+            res.send('Aww! See ya later!');
+         }
+      });
+   } else {
+      res.end();
+   }
+});
+
+router.post('/login', mw.validateLogin, (req, res) => {
+   const user = req.user;
+   try {
+      res.status(200).json({ message: `Welcome ${user.username}!` });
    } catch (error) {
       res.status(500).json('Oops! We missed that. Hang on, let\'s fix it together');
    };
-})
+});
 
 module.exports = router;
